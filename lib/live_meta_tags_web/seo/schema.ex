c17d48cc2457f65ac1ed4549d8c1cbd92cf981b0
snewcomer <snewcomer24@gmail.com>
2019-10-20 19:46:41 -0700

    put schema info in view

--- lib/live_meta_tags_web/seo/schema.ex
+++ lib/live_meta_tags_web/seo/schema.ex
@@ -8,19 +8,21 @@ defmodule LiveMetaTagsWeb.Seo.Schema do
         {
           "@context": "http://schema.org",
           "@type": "MusicAlbum",
-          "name": <%= album.title %>,
-          "description": <%= album.description %>,
+          "name": "<%= album.title %>",
+          "description": "<%= album.description %>",
           "tracks": [
-            {
-              "@type": "MusicRecording",
-              "name": "My Track",
-              "audio": {"@type":"AudioObject"},
-              "offers": {
-                "@type": "Offer",
-                "price": "$1.29"
-              },
-              "duration": "PT2M50S"
-            }
+            <%= for track <- album.tracks do %>
+              {
+                "@type": "MusicRecording",
+                "name": "<%= track.name %>",
+                "audio": {"@type":"AudioObject"},
+                "offers": {
+                  "@type": "Offer",
+                  "price": "<%= track.price %>"
+                },
+                "duration": "<%= track.duration %>"
+              }
+            <% end %>
           ]
         }
       </script>
